20f6f3f4f007cc2999e466730f8bbd9569fcd2a2
Nitesh Kumar <151807684+nitesh6516@users.noreply.github.com>
2026-02-06 11:07:58 +0530

    Array.java

--- Array.java
+++ Array.java
@@ -1,1 +1,43 @@
 
+import java.util.Scanner;
+
+public class Array {
+    public static int findSecondLargest(int[] arr){
+
+        
+        int Largest = arr[0];
+        int SecondLargest = arr[0];
+
+        for(int i=1;i<=arr.length;i++){
+         
+            if(arr[i] > Largest){
+                SecondLargest = Largest;
+                Largest = arr[i];
+        }
+            else if (arr[i] > SecondLargest && arr[i] != Largest){
+                SecondLargest = arr[i];
+            }
+        }
+        return SecondLargest;
+       
+    }
+}
+
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    int arr[] = new int[n];
+
+    for(int i=0;i<n;i++){
+        arr[i] = sc.nextInt();
+
+    }
+
+    int statement = findSecondLargest(arr); 
+
+    System.out.println("SecondLargest" + statement);
+    sc.close();
+}
+
+
